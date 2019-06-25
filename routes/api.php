a122387d6d8c98c9863248d10712b9addc3dd329
HARSHAN-MAD <harshanmadhurangaw@gmail.com>
2019-06-25 23:51:22 +0530

    update api

--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,24 @@ Route::get('articles', function() {
     // this will return a JSON structure. This will be cleaned up later.
     return Article::all();
 });
+
+Route::get('articles/{id}', function($id) {
+    return Article::find($id);
+});
+
+Route::post('articles', function(Request $request) {
+    return Article::create($request->all);
+});
+
+Route::put('articles/{id}', function(Request $request, $id) {
+    $article = Article::findOrFail($id);
+    $article->update($request->all());
+
+    return $article;
+});
+
+Route::delete('articles/{id}', function($id) {
+    Article::find($id)->delete();
+
+    return 204;
+});
